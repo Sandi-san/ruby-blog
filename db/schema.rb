a3bfd831fa1b569bd5ece7d15b1c7b611ecedd5c
Sandi-san <nicksrebotsandi@gmail.com>
2021-02-13 15:28:27 +0100

    String Body + scaffold news

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210209113931) do
+ActiveRecord::Schema.define(version: 20210213135816) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
