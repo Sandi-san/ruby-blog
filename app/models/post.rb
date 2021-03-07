c78c664af02c906751eb88d976303cb238c0250f
Sandi-san <nicksrebotsandi@gmail.com>
2021-03-07 16:55:12 +0100

    Dodaja komentiranja

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user #User id od userja (glej user.rb)
+    acts_as_commontable dependent: :destroy
 
     validates :title, presence: true
     validates :body, presence: true
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,14 +2,16 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require("webpack")
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-
-$: 'jquery',
-
-jQuery: 'jquery',
-
-Popper: ['popper.js', 'default']
-
+environment.plugins.append(
+    'Provide', 
+    new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default']
 }))
+
+environment.config.set('resolve.alias', {jquery: 'jquery/src/jquery'});
 
 module.exports = environment
